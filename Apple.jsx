d63b1049e0222593253cd85ec3b156a1f860dd88
Harshini-Gunasekar <146466317+Harshini-Gunasekar@users.noreply.github.com>
2024-12-29 22:09:33 +0530

    Add files via upload

---
+++ Apple.jsx
@@ -0,0 +1,16 @@
+import {Component} from "react";
+
+
+
+class Apple extends Component{
+    render(){
+        return(
+            <div>
+                <h1>apple</h1>
+
+            </div>
+        )
+    }
+}
+
+export default Apple
---
+++ Form.jsx
@@ -0,0 +1,16 @@
+function Form(){
+    let name="Harshini";
+    let dept="CSD";
+    let dob="20.03.2005"
+
+    return(
+        <div>
+            <h2>Form belongs to {name}</h2>
+            <h2>{name} is studying in {dept}</h2>
+            <h3>{name} date of birth {dob}</h3>
+        </div>
+    )
+
+}
+
+export default Form
